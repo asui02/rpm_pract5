d7d4d6f7451dcf83a37df12c25e16efc825dd166
asui02 <uhaato@gmail.com>
2021-11-15 21:37:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pr5/Chili.java
@@ -0,0 +1,14 @@
+package com.example.pr5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Chili extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chili);
+    }
+}
